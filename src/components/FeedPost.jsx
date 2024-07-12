609b240475ad5f0797e7c7dde368aaf1f7c2d4f4
aong13 <oaoa13@pukyong.ac.kr>
2024-07-13 03:52:46 +0900

    design: feedPost css 수정 및 좋아요 토글

--- src/components/FeedPost.jsx
+++ src/components/FeedPost.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Dimensions, View, TouchableOpacity, Image, Text, StyleSheet } from 'react-native';
 
 const more_icon = require('../assets/icons/more.png');
@@ -10,58 +10,71 @@ const { width } = Dimensions.get('window');
 
 
 const FeedPost = ({ id, profileImg, nickname, likeNum, commentNum, thumbnail, contents, navigation }) => {
-    console.log("id: ", id);
+    const [isLiked, setIsLiked] = useState(false);
     
-    const handleImgPress = () => {
-        console.log("클릭된 id: ", id);
-        navigation.navigate('FeedDetail', { id }); //home으로부터 받은 id 전달
-    }; 
+    const goToUserProfile = () => {
+        //댓글 작성자의 프로필로 이동
+    }
     const handleLikePress = () => {
         //좋아요 클릭시 좋아요 색 변경
+        setIsLiked(!isLiked);
     };
+    const handleImgPress = () => {
+        console.log("클릭된id: ", id);
+        navigation.navigate('FeedDetail', { id }); //home으로부터 받은 id 전달
+    }; 
     const handleCommentPress = () => {
         //댓글 아이콘 클릭시 댓글만 보이도록
     };
       
   return (
     <View style={styles.postContainer}>
-    <View style={styles.postHeader}>
-        <View style={{ flexDirection: 'row', alignItems: 'center', gap: 4 }}>
-        <TouchableOpacity>
-            <Image source={{ uri: profileImg}} style={{ width: 32, height: 32 }} />
+        <View style={styles.postHeader}>
+            <View style={{ flexDirection: 'row', alignItems: 'center', gap: 4 }}>
+                <TouchableOpacity>
+                    <Image source={{ uri: profileImg}} style={{ width: 32, height: 32 }} />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text style={styles.nicknameText}>{nickname}</Text> 
+                </TouchableOpacity>
+                </View>
+                <TouchableOpacity>
+                    <Image source={more_icon} style={{ width: 24, height: 24 }} />
+                </TouchableOpacity>
+            </View>
+
+        <TouchableOpacity 
+            onPress={() => handleImgPress()}
+            activeOpacity={0.8}>
+            
+            <Image 
+                source={{ uri: thumbnail }} //첫번째만 가져오기!!
+                style={styles.thumbnailImg}
+                resizeMode='contain'/>
         </TouchableOpacity>
-        <TouchableOpacity>
-            <Text style={{ fontSize: 16, lineHeight: 19.97 }}>{nickname}</Text> 
-        </TouchableOpacity>
+
+        <View style={styles.reactionsContainer}>
+            <View style={styles.reactionWrapper}>
+                <TouchableOpacity
+                    onPress={() => handleLikePress()}>
+                    <Image source={isLiked ? heart_fill_icon : heart_icon} style={{ width: 24, height: 24 }} />
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={() => handleCommentPress()}>
+                    <Text>{likeNum}</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.reactionWrapper}>
+                <TouchableOpacity>
+                    <Image source={comment_icon} style={{ width: 24, height: 24 }} />
+                </TouchableOpacity>
+                <Text>{commentNum}</Text>
+            </View>
         </View>
-        <TouchableOpacity>
-            <Image source={more_icon} style={{ width: 24, height: 24 }} />
-        </TouchableOpacity>
-    </View>
-    <TouchableOpacity onPress={() => handleImgPress()} >
-        <Image 
-            source={{ uri: thumbnail }} //첫번째만 가져오기!!
-            style={styles.thumbnailImg}
-            resizeMode='contain'/>
-    </TouchableOpacity>
-    <View style={styles.reactionsContainer}>
-        <TouchableOpacity
-            onPress={() => handleLikePress()}>
-            <Image source={heart_icon} style={{ width: 24, height: 24 }} />
-        </TouchableOpacity>
-        <TouchableOpacity
-            onPress={() => handleCommentPress()}>
-            <Text>{likeNum}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-            <Image source={comment_icon} style={{ width: 24, height: 24 }} />
-        </TouchableOpacity>
-        <Text>{commentNum}</Text>
-    </View>
-    <View style={styles.postContentContainer}>
-        <Text>{nickname}</Text>
-        <Text style={{color: '#4F4F4F' }}>{contents}</Text>
-    </View>
+        <View style={styles.postContentContainer}>
+            <Text style={styles.contentsText}>{contents}</Text>
+            <Text style={styles.timeText}>{"2시간 전"}</Text>
+        </View>
 </View>
   )
 }
@@ -69,17 +82,23 @@ const FeedPost = ({ id, profileImg, nickname, likeNum, commentNum, thumbnail, co
 
 const styles = StyleSheet.create({
     postContainer:{
-        paddingVertical: 24, 
+        paddingVertical: 16, 
         borderBottomWidth:0.2,
         borderBottomColor:'#d0d0d0',
-        paddingBottom: 24, 
     },
+    //글 상단
     postHeader:{
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
         marginHorizontal: 16,
-        marginBottom: 10 
+        marginBottom: 16,
+    },
+    nicknameText:{
+        fontSize: 14,
+        fontWeight: "bold",
+        lineHeight: 20.27,
+        color: '#3A3A3A'
     },
     thumbnailImg:{
         width: width-24, 
@@ -91,15 +110,31 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         gap:10,
         alignItems: 'center', 
-        paddingHorizontal: 16, 
+        marginHorizontal: 16, 
         paddingVertical: 8,
-        borderBottomWidth:0.2,
-        borderBottomColor:'#d0d0d0',
+        marginTop: 4,
+    },
+    reactionWrapper:{
+        flexDirection: 'row',
+        gap:4,
+        alignItems: 'center', 
     },
     postContentContainer:{
-        paddingVertical: 8,
-        marginHorizontal: 16,
-        gap: 4,
-    }
+        marginHorizontal: 20,
+        gap: 6,
+    },
+    timeText: {
+        fontSize: 13,
+        lineHeight:18.82,
+        letterSpacing: 0.03,
+        color: '#A5A5A5',
+    },
+    contentsText:{
+        fontSize: 15,
+        lineHeight: 21.72,
+        letterSpacing: 0.03,
+        textAlign: 'left',
+        color: '#3A3A3A'   
+    },
 });
 export default FeedPost
