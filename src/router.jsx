629ff7b34be3a1b0d67c3e18383f89a396ae48bd
aong13 <oaoa13@pukyong.ac.kr>
2024-07-14 08:15:40 +0900

    feat: SearchList UI
    - 임시의 UI만 제작

--- src/router.jsx
+++ src/router.jsx
@@ -3,13 +3,16 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Splash from './pages/Splash';
-
 import Home from './pages/mainTab/Home';
 import Search from './pages/mainTab/Search';
 import Dm from './pages/mainTab/Dm';
 import MyPage from './pages/mainTab/MyPage';
 import Add from './pages/mainTab/Add';
+
 import FeedDetail from './pages/FeedDetail';
+import SearchList from './pages/SearchList';
+import AddFeed from './pages/AddFeed'; 
+
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import CustomBottomTap from './components/CustomBottomTap';
@@ -17,12 +20,12 @@ import CustomBottomTap from './components/CustomBottomTap';
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const renderTapbar = (props) => <CustomBottomTap {...props} />;
+const renderTabBar = (props) => <CustomBottomTap {...props} />;
 
 const MainTab = () => {
   return (
     <Tab.Navigator
-      tabBar={renderTapbar}
+      tabBar={renderTabBar}
       screenOptions={{
         headerShown: false,
       }}
@@ -45,9 +48,11 @@ const Router = () => {
     >
       <Stack.Screen name="Splash" component={Splash} />
       <Stack.Screen name="FeedDetail" component={FeedDetail} />
-      <Stack.Screen name="MainTab" component={MainTab} />
+      <Stack.Screen name="SearchList" component={SearchList} />
+      <Stack.Screen name="AddFeed" component={AddFeed} /> 
       <Stack.Screen name="LoginPage" component={LoginPage} />
       <Stack.Screen name="SignupPage" component={SignupPage} />
+      <Stack.Screen name="MainTab" component={MainTab} />
     </Stack.Navigator>
   );
 };
