d24c7d4c69228ebcca190082c24b17c6e65d7121
aong13 <oaoa13@pukyong.ac.kr>
2024-07-14 09:22:07 +0900

    feat: 게시글 업로드 구현

--- src/apis/Feed.js
+++ src/apis/Feed.js
@@ -3,18 +3,19 @@ import API from './index'
 
 export const getFeed = async (page, pageSize) => {
   try {
-    const res = await API.get('/feed', {
-      params: {
-        page,
-        pageSize,
+      const res = await API.get('/feed', {
+          params: {
+            page,
+            pageSize,
       },
-    });
-    return res.data;
+      });
+      return res.data;
   } catch (error) {
-    console.error('피드목록 조회 실패:', error.res); 
-    throw error;
+      console.error('피드목록 조회 실패:', error.res); 
+      throw error;
   }
 };
+
 export const getSearchTag = async (searchTag, page, pageSize) => {
   try {
       const res = await API.get('/feed/search', {
@@ -27,6 +28,34 @@ export const getSearchTag = async (searchTag, page, pageSize) => {
       return res.data;
   } catch (error) {
       console.error('태그 검색 실패:', error.response);
+      throw error;
+  }
+};
+
+export const createFeed = async (content, tags, images) => {
+  const feedRequest = {
+    content: content,
+    tags: tags,
+  };
+  const formData = new FormData();
+  formData.append('feedRequest', JSON.stringify(feedRequest));
+
+  images.map((image, index) => {
+     formData.append('image', {
+          name: image.filename || `image_${index}.jpg`,
+          uri: image.uri,
+          type: image.type || 'image/jpeg',
+    });
+  });
+  console.log("formData:", formData);
+  try {
+      const res = await API.post('/feed', formData,{
+          headers:{
+              'Content-Type': 'multipart/form-data',
+      }});
+    return res.data;
+  } catch (error) {
+      console.error('피드 작성 실패:', error.response);
       throw error;
   }
 };
--- src/pages/AddFeed.jsx
+++ src/pages/AddFeed.jsx
@@ -1,102 +1,110 @@
 import React, { useState } from 'react';
 import { SafeAreaView, View, Text, Image, StyleSheet, TouchableOpacity, Dimensions, TextInput, Keyboard, ScrollView } from 'react-native';
 import HashTags from '../components/HashTags';
+import { createFeed } from '../apis/Feed';
+
 const { width } = Dimensions.get('window');
 
 const back_icon = require('../assets/icons/back.png');
 const hashTagIcon = require('../assets/icons/hashtag.png');
 const closeIcon = require('../assets/icons/close.png');
 
-// 이전 페이지에서 받아올 예정인 이미지 데이터
-const receivedImages = [
-  { id: 1, uri: 'https://via.placeholder.com/150' },
-  { id: 2, uri: 'https://via.placeholder.com/150' },
-  { id: 3, uri: 'https://via.placeholder.com/150' },
-  { id: 4, uri: 'https://via.placeholder.com/150' },
-  { id: 5, uri: 'https://via.placeholder.com/150' },
-];
 
-const AddFeed = ({ navigation }) => {
-  const [uploadingImage, setUploadingImage] = useState(receivedImages);
-  const [keywords, setKeywords] = useState([]);
-  const [keyword, setKeyword] = useState('');
-  const [content, setContent] = useState('');
+const AddFeed = ({ navigation, route }) => {
+    const { selectedPhotos } = route.params;
+    const [uploadingImage, setUploadingImage] = useState(selectedPhotos || []);
+    const [keyword, setKeyword] = useState('');
+    const [keywords, setKeywords] = useState([]);
+    const [content, setContent] = useState('');
+    
+    const handleUpload = async () => {
+        try {
+            await createFeed(content, keywords, uploadingImage);
+            alert('피드가 성공적으로 업로드되었습니다.');
+            navigation.goBack();
+        } catch (error) {
+            console.log("addFeed.jsx에서 보내주는 uploadImag:", uploadingImage)
+            console.log("선택한 이미지:", selectedPhotos)
+            alert('피드 업로드에 실패했습니다.');
+        }
+    };
 
-  const handleDelete = (id) => {
-    const updatedData = uploadingImage.filter(item => item.id !== id);
-    setUploadingImage(updatedData);
-  };
+    const handleDelete = (uri) => {
+      const updatedData = uploadingImage.filter(item => item.uri !== uri);
+      setUploadingImage(updatedData);
+    };
 
-  const handleAddKeyword = () => {
-    if (keyword.trim().length > 0) {
-      setKeywords([...keywords, keyword.trim()]);
-      setKeyword('');
-      Keyboard.dismiss();
-    }
-  };
+    const handleAddKeyword = () => {
+        if (keyword.trim().length > 0) {
+            setKeywords([...keywords, keyword.trim()]);
+            setKeyword('');
+        }
+    };
 
-  const handleDeleteKeyword = (tag) => {
-    const updatedKeywords = keywords.filter((keyword) => keyword !== tag);
-    setKeywords(updatedKeywords);
-  };
-
-  return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: '#FFF' }}>
-      <View style={styles.headerWrapper}>
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Image source={back_icon} style={styles.headerIcon} />
-        </TouchableOpacity>
-        <Text style={{ fontSize: 15, color: '#3A3A3A', fontWeight: 'bold' }}>데일리 피드 작성</Text>
-        <TouchableOpacity>
-          <Text style={{ fontSize: 14, color: '#2F80ED', fontWeight: 'bold' }}>업로드</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={{ margin: 16, width: width - 32 }}>
-        <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.imageWrapper}>
-          {uploadingImage.map(item => (
-            <View style={{ paddingHorizontal: 4 }} key={item.id}>
-              <Image source={{ uri: item.uri }} style={{ width: 100, height: 100, borderRadius: 4 }} />
-              <TouchableOpacity style={styles.deleteBtn} onPress={() => handleDelete(item.id)}>
-                <Image source={closeIcon} />
-              </TouchableOpacity>
+    const handleDeleteKeyword = (tag) => {
+        const updatedKeywords = keywords.filter((keyword) => keyword !== tag);
+        setKeywords(updatedKeywords);
+    };
+    
+    return (
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#FFF'}}>
+            <View style={styles.headerWrapper}>
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                    <Image source={back_icon} style={styles.headerIcon} />
+                </TouchableOpacity>
+                <Text style={{ fontSize: 15, color: '#3A3A3A', fontWeight: 'bold' }}>데일리 피드 작성</Text>
+                <TouchableOpacity
+                    onPress={()=> handleUpload()}>
+                    <Text style={{ fontSize: 14, color: '#2F80ED', fontWeight: 'bold' }}>업로드</Text>
+                </TouchableOpacity>
             </View>
-          ))}
-        </ScrollView>
-
-        <TextInput
-          style={styles.contentInput}
-          placeholder="게시글을 입력하세요. 오늘은 어떤 운동을 했나요?"
-          value={content}
-          onChangeText={(text) => setContent(text)}
-          multiline
-        />
-
-        <Text style={styles.heading}>키워드 추가</Text>
-        <View style={styles.searchWrapper}>
-          <View style={styles.searchSection}>
-            <TouchableOpacity style={styles.hashTagIconStyle}>
-              <Image source={hashTagIcon} style={{ width: 18, height: 18 }} />
-            </TouchableOpacity>
-            <TextInput
-              placeholder='태그를 입력하세요.'
-              returnKeyType='search'
-              spellCheck={false}
-              autoCorrect={false}
-              autoCapitalize='none'
-              value={keyword}
-              onChangeText={(text) => setKeyword(text)}
-              onSubmitEditing={handleAddKeyword}
-              allowFontScaling={false}
-              style={styles.inputStyle}
-            />
-          </View>
-        </View>
-        <View style={styles.hashTagWrapper}>
-            <HashTags tagList={keywords} onDelete={handleDeleteKeyword} />
-        </View>
-      </View>
-    </SafeAreaView>
-  );
+            <View style={{ margin:16, width: width-32}}>
+                <ScrollView horizontal showsHorizontalScrollIndicator={false} style={styles.imageWrapper}>
+                    {uploadingImage.map(item => (
+                        <View style={{paddingHorizontal: 4}}key={item.uri}>
+                            <Image source={{ uri: item.uri }} style={{ width: 100, height: 100, borderRadius: 4 }} />
+                                <TouchableOpacity style={styles.deleteBtn} onPress={() => handleDelete(item.uri)} >
+                                    <Image source={closeIcon} />
+                                </TouchableOpacity>
+                        </View>
+                    ))}
+                </ScrollView>
+        
+                <TextInput
+                    style={styles.contentInput}
+                    placeholder="게시글을 입력하세요. 오늘은 어떤 운동을 했나요?"
+                    value={content}
+                    onChangeText={(text) => setContent(text)}
+                    multiline
+                />
+                    
+                <Text style={styles.heading}>키워드 추가</Text>
+                
+                <View style={styles.searchWrapper}>
+                    <View style={styles.searchSection}>
+                        <TouchableOpacity style={styles.hashTagIconStyle}>
+                            <Image source={hashTagIcon} style={{ width: 18, height: 18 }} />
+                        </TouchableOpacity>
+                        <TextInput
+                            placeholder='태그를 입력하세요.'
+                            returnKeyType='search'
+                            spellCheck={false}
+                            autoCorrect={false}
+                            autoCapitalize='none'
+                            value={keyword}
+                            onChangeText={(text) => setKeyword(text)}
+                            onSubmitEditing={handleAddKeyword}
+                            allowFontScaling={false}
+                            style={styles.inputStyle}
+                        />
+                    </View>
+                </View>
+                <View style={styles.hashTagWrapper}>
+                    <HashTags tagList={keywords} onDelete={handleDeleteKeyword} />
+                </View>
+            </View>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({
