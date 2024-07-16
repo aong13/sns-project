31a654eb67410fc9f87bd8c4709f81a998ed4abe
aong13 <oaoa13@pukyong.ac.kr>
2024-07-16 11:32:21 +0900

    feat: 피드 업로드 시 사진 여러장 선택택

--- src/pages/mainTab/Add.jsx
+++ src/pages/mainTab/Add.jsx
@@ -1,41 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import { SafeAreaView, View, Text, Image, FlatList, TouchableOpacity, useWindowDimensions, PermissionsAndroid, Platform } from 'react-native';
-import {CameraRoll} from '@react-native-camera-roll/camera-roll'; 
+import { CameraRoll } from '@react-native-camera-roll/camera-roll';
 
 const Add = ({ navigation }) => {
     const [images, setImages] = useState([]);
+    const [selectedPhotos, setSelectedPhotos] = useState([]);
     const [selectedPhoto, setSelectedPhoto] = useState();
     const [selectedIndex, setSelectedIndex] = useState();
 
     const { width } = useWindowDimensions();
 
     useEffect(() => {
-        requestPermission().then(() => {
+//        requestPermission().then(() => {
             FetchImages();
-        });
+//        });
     }, []);
 
-    const requestPermission = async () => {
-        if (Platform.OS === 'android') {
-            try {
-                const granted = await PermissionsAndroid.request(
-                    PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
-                    {
-                        title: "Access to Photos",
-                        message: "We need to access your photos to display them.",
-                        buttonNeutral: "Ask Me Later",
-                        buttonNegative: "Cancel",
-                        buttonPositive: "OK"
-                    }
-                );
-                if (granted !== PermissionsAndroid.RESULTS.GRANTED) {
-                    console.log("READ_EXTERNAL_STORAGE permission denied");
-                }
-            } catch (err) {
-                console.warn(err);
-            }
-        }
-    };
+    // const requestPermission = async () => {
+    //     if (Platform.OS === 'android') {
+    //         try {
+    //             const granted = await PermissionsAndroid.request(
+    //                 PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+    //                 {
+    //                     title: "Access to Photos",
+    //                     message: "We need to access your photos to display them.",
+    //                     buttonNeutral: "Ask Me Later",
+    //                     buttonNegative: "Cancel",
+    //                     buttonPositive: "OK"
+    //                 }
+    //             );
+    //             if (granted !== PermissionsAndroid.RESULTS.GRANTED) {
+    //                 console.log("READ_EXTERNAL_STORAGE permission denied");
+    //             }
+    //         } catch (err) {
+    //             console.warn(err);
+    //         }
+    //     }
+    // };
 
     const FetchImages = async () => {
         try {
@@ -44,7 +45,6 @@ const Add = ({ navigation }) => {
                 assetType: 'Photos',
                 groupTypes: 'All'
             });
-            console.log("res:", res);
             if (res.edges.length > 0) {
                 setSelectedPhoto(res.edges[0].node.image);
                 setSelectedIndex(0);
@@ -55,15 +55,27 @@ const Add = ({ navigation }) => {
         }
     };
 
+    //여러장 선택하기
+    const handleSelectPhoto = (item) => {
+        const isSelected = selectedPhotos.some(photo => photo.uri === item.uri);
+        if (isSelected) {
+            setSelectedPhotos(selectedPhotos.filter(photo => photo.uri !== item.uri));
+        } else {
+            setSelectedPhotos([...selectedPhotos, item]);
+        }
+    };
+
     const renderItem = ({ item, index }) => {
+        const isSelected = selectedPhotos.some(photo => photo.uri === item.uri);
         return (
             <TouchableOpacity 
                 style={{ borderWidth: 1, borderColor: '#FFF' }}
                 onPress={() => {
+                    handleSelectPhoto(item);
                     setSelectedPhoto(item);
                     setSelectedIndex(index);
                 }}>
-                {selectedIndex === index && (
+                {isSelected && (
                     <View style={{ position: 'absolute', right: 8, top: 2, width: 20, height: 20, borderWidth: 1, borderColor: '#000', borderRadius: 20, zIndex: 2 }}>
                         <View style={{ width: 20, height: 20, borderRadius: 10, backgroundColor: 'green' }} />
                     </View>
@@ -80,7 +92,7 @@ const Add = ({ navigation }) => {
                     <Text style={{ fontSize: 24, color: '#3A3A3A', fontWeight: 'bold' }}>X</Text>
                 </TouchableOpacity>
                 <Text style={{ fontSize: 15, color: '#3A3A3A', fontWeight: 'bold' }}>새 게시물</Text>
-                <TouchableOpacity onPress={() => navigation.navigate('AddFeed')}>
+                <TouchableOpacity onPress={() => navigation.navigate('AddFeed', { selectedPhotos })}>
                     <Text style={{ fontSize: 14, color: '#2F80ED', fontWeight: 'bold' }}>다음</Text>
                 </TouchableOpacity>
             </View>
